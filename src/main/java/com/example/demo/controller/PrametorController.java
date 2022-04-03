0c44a913637768d3b813607be94b7a2684c47844
Ryosuke Takahashi <r-takahashi@image-pit.com>
2022-04-03 19:33:13 +0900

    パラーメータ取得（確認）の実装

--- src/main/java/com/example/demo/controller/PrametorController.java
+++ src/main/java/com/example/demo/controller/PrametorController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("param")
 @Controller
@@ -10,5 +13,17 @@ public class PrametorController {
 	@GetMapping
 	public String form() {
 		return "param/form";
+	}
+	@PostMapping("confirm")
+	public String confirm(
+			@RequestParam String itemName, 
+			@RequestParam Integer itemPrice,
+			Model model
+	) {
+		System.out.println("itemName:"+itemName);
+		System.out.println("itemPrice:"+itemPrice);
+		model.addAttribute("itemName", itemName);
+		model.addAttribute("itemPrice", itemPrice);
+		return "param/confirm";
 	}
 }
