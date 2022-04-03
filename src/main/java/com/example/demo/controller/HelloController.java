2de75cb4c2bbd8ee576dc383072b131342f63af2
Ryosuke Takahashi <r-takahashi@image-pit.com>
2022-04-03 19:17:15 +0900

    @PostMappingとリダイレクトの確認

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,13 @@ public class HelloController {
 	@GetMapping
 	public String hello() {
 		return "hello/hello";
+	}
+	@PostMapping
+	public String post() {
+		return "redirect:/hello/redirect";
+	}
+	@GetMapping("redirect")
+	public String redirect() {
+		return "hello/redirect";
 	}
 }
