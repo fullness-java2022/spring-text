6d3dc42308c0eaedcbf053671eb47bc3b9292ddc
Ryosuke Takahashi <r-takahashi@image-pit.com>
2022-04-04 13:01:20 +0900

    相関バリデーションの実装

--- src/main/java/com/example/demo/controller/SessionController.java
+++ src/main/java/com/example/demo/controller/SessionController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.form.ItemForm;
+import com.example.demo.form.ItemFormValidator;
 
 @SessionAttributes("itemForm")
 @RequestMapping("session")
@@ -22,6 +26,12 @@ public class SessionController {
 	public ItemForm setup() {
 		return new ItemForm();
 	}
+	@Autowired private ItemFormValidator validator;
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(validator);
+	}
+	
 	@GetMapping("form")
 	public String form() {
 		return "session/form";
---
+++ src/main/java/com/example/demo/form/ItemFormValidator.java
@@ -0,0 +1,22 @@
+package com.example.demo.form;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class ItemFormValidator implements Validator{
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return ItemForm.class.isAssignableFrom(clazz);
+	}
+	@Override
+	public void validate(Object target, Errors errors) {
+		ItemForm form = (ItemForm)target;
+		// 相関チェックする項目がnullだヌルポの例外が発生するので早期退出させる
+		if(form.getItemName() == null || form.getItemPrice() == null) return;
+		if(form.getItemPrice() < 1000 && form.getItemName().equals("パソコン")) {
+			errors.reject("com.example.demo.form.ItemForm.message");
+		}
+	}
+}
